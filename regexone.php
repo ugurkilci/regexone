38ee71565e4ce8f84e1e0384a3f02ba2d7e7c122
Uğur KILCI <ugurbocugu8@gmail.com>
2017-05-22 23:47:29 +0200

    Update regexone.php

--- regexone.php
+++ regexone.php
@@ -1,12 +1,12 @@
 <?php
-  function urlregex($text){ // Run url
+  function urlregexone($text){ // Run url
 		$pattern = '((http.*\.)(.*)(/))';
 		$replace = '<a href="$0" target="_blank" title="$0">$0</a><br />';
 		
 		echo preg_replace($pattern, $replace, $text); // print
 	}
   
-  function imgregex($text){ // Run image
+  function imgregexone($text){ // Run image
 		$pattern = '((http.*\.)(www?|)(jpg|jpeg|png|svg|gif))';
 		$replace = '<a href="$0" target="_blank" title="$0"><img src="$0" class="img-thumbnail" style="width:100px;" alt="$0"></a><br />';
 		 
